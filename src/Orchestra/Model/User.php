orchestral/model

commit bb61cc993854b0a23e0cad6597c2b151e4e96f1a

Use User::whereHas('roles') to get filtered search instead of doing join query.
Signed-off-by: crynobone <crynobone@gmail.com>

src/Orchestra/Model/User.php
@@ -60,8 +60,9 @@ class User extends Eloquent implements UserInterface, RemindableInterface, Recip
         $query->with('roles')->whereNotNull('users.id');
 
         if (! empty($roles)) {
-            $query->join('user_role', 'users.id', '=', 'user_role.user_id')
-                ->whereIn('user_role.role_id', $roles);
+            $query->whereHas('roles', function ($query) use ($roles) {
+                $query->whereIn('id', $roles);
+            });
         }
 
         if (! empty($keyword)) {

tests/UserTest.php
@@ -266,10 +266,13 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $roles   = array('admin');
 
         $query = m::mock('\Illuminate\Database\Eloquent\Builder');
+        $query->shouldReceive('whereIn')->once()->with('id', $roles)->andReturn(null);
         $query->shouldReceive('with')->once()->with('roles')->andReturn($query)
             ->shouldReceive('whereNotNull')->once()->with('users.id')->andReturn($query)
-            ->shouldReceive('join')->once()->with('user_role', 'users.id', '=', 'user_role.user_id')->andReturn($query)
-            ->shouldReceive('whereIn')->once()->with('user_role.role_id', $roles)->andReturn(null)
+            ->shouldReceive('whereHas')->once()->with('roles', m::type('Closure'))
+                ->andReturnUsing(function ($n, $c) use ($query) {
+                    $c($query);
+                })
             ->shouldReceive('orWhere')->once()->with('email', 'LIKE', $search)->andReturn($query)
             ->shouldReceive('orWhere')->once()->with('fullname', 'LIKE', $search)->andReturn(null)
             ->shouldReceive('where')->once()->with(m::type('Closure'))->andReturnUsing(function ($q) use ($query, $keyword) {

commit 15f5c85c450d9d527e14ec166f3c3a7d55789439

Fixed regression bug from bb61cc9 commit and update changelog.
Signed-off-by: crynobone <crynobone@gmail.com>

src/Orchestra/Model/User.php
@@ -61,7 +61,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface, Recip
 
         if (! empty($roles)) {
             $query->whereHas('roles', function ($query) use ($roles) {
-                $query->whereIn('id', $roles);
+                $query->whereIn('roles.id', $roles);
             });
         }
 

tests/UserTest.php
@@ -266,13 +266,13 @@ class UserTest extends \PHPUnit_Framework_TestCase
         $roles   = array('admin');
 
         $query = m::mock('\Illuminate\Database\Eloquent\Builder');
-        $query->shouldReceive('whereIn')->once()->with('id', $roles)->andReturn(null);
         $query->shouldReceive('with')->once()->with('roles')->andReturn($query)
             ->shouldReceive('whereNotNull')->once()->with('users.id')->andReturn($query)
             ->shouldReceive('whereHas')->once()->with('roles', m::type('Closure'))
                 ->andReturnUsing(function ($n, $c) use ($query) {
                     $c($query);
                 })
+            ->shouldReceive('whereIn')->once()->with('roles.id', $roles)->andReturn(null)
             ->shouldReceive('orWhere')->once()->with('email', 'LIKE', $search)->andReturn($query)
             ->shouldReceive('orWhere')->once()->with('fullname', 'LIKE', $search)->andReturn(null)
             ->shouldReceive('where')->once()->with(m::type('Closure'))->andReturnUsing(function ($q) use ($query, $keyword) {