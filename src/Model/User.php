ba7f9d925447c81e87f7d67bb6944a3e19be1779
crynobone <crynobone@gmail.com>
2014-05-03 20:47:39 +0800

    Add use UserTrait and RemindableTrait.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Model/User.php
+++ src/Model/User.php
@@ -1,6 +1,8 @@
 <?php namespace Orchestra\Model;
 
+use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model as Eloquent;
@@ -12,8 +14,7 @@ use Orchestra\Support\Traits\QueryFilterTrait;
 
 class User extends Eloquent implements UserInterface, RemindableInterface, RecipientInterface
 {
-    use SoftDeletingTrait;
-    use QueryFilterTrait;
+    use UserTrait, RemindableTrait, SoftDeletingTrait, QueryFilterTrait;
 
     /**
      * Available user status as constant.
@@ -76,67 +77,6 @@ class User extends Eloquent implements UserInterface, RemindableInterface, Recip
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = Hash::make($value);
-    }
-
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
-    public function getAuthIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
-
-    /**
-     * Get the token value for the "remember me" session.
-     *
-     * @return string
-     */
-    public function getRememberToken()
-    {
-        return $this->remember_token;
-    }
-
-    /**
-     * Set the token value for the "remember me" session.
-     *
-     * @param  string  $value
-     * @return void
-     */
-    public function setRememberToken($value)
-    {
-        $this->remember_token = $value;
-    }
-
-    /**
-     * Get the column name for the "remember me" token.
-     *
-     * @return string
-     */
-    public function getRememberTokenName()
-    {
-        return 'remember_token';
-    }
-
-    /**
-     * Get the e-mail address where password reminders are sent.
-     *
-     * @return string
-     */
-    public function getReminderEmail()
-    {
-        return $this->email;
     }
 
     /**
