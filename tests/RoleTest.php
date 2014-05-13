bbe43a925b3676e20fc2666cecedae0561dc7dc4
crynobone <crynobone@gmail.com>
2014-05-13 09:15:57 +0800

    Utilize Orchestra\Support\Traits\EloquentConnectionTrait.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/RoleTest.php
+++ tests/RoleTest.php
@@ -5,22 +5,7 @@ use Orchestra\Model\Role;
 
 class RoleTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * Set mock connection
-     */
-    protected function addMockConnection($model)
-    {
-        $resolver = m::mock('Illuminate\Database\ConnectionResolverInterface');
-        $model->setConnectionResolver($resolver);
-        $resolver->shouldReceive('connection')
-            ->andReturn(m::mock('Illuminate\Database\Connection'));
-        $model->getConnection()
-            ->shouldReceive('getQueryGrammar')
-                ->andReturn(m::mock('Illuminate\Database\Query\Grammars\Grammar'));
-        $model->getConnection()
-            ->shouldReceive('getPostProcessor')
-                ->andReturn(m::mock('Illuminate\Database\Query\Processors\Processor'));
-    }
+    use \Orchestra\Support\Traits\EloquentConnectionTrait;
 
     /**
      * Setup the test environment.
--- tests/UserMetaTest.php
+++ tests/UserMetaTest.php
@@ -5,22 +5,7 @@ use Orchestra\Model\UserMeta;
 
 class UserMetaTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * Set mock connection
-     */
-    protected function addMockConnection($model)
-    {
-        $resolver = m::mock('Illuminate\Database\ConnectionResolverInterface');
-        $model->setConnectionResolver($resolver);
-        $resolver->shouldReceive('connection')
-            ->andReturn(m::mock('Illuminate\Database\Connection'));
-        $model->getConnection()
-            ->shouldReceive('getQueryGrammar')
-                ->andReturn(m::mock('Illuminate\Database\Query\Grammars\Grammar'));
-        $model->getConnection()
-            ->shouldReceive('getPostProcessor')
-                ->andReturn(m::mock('Illuminate\Database\Query\Processors\Processor'));
-    }
+    use \Orchestra\Support\Traits\EloquentConnectionTrait;
 
     /**
      * Teardown the test environment.
