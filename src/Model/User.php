302768b172df2b31880da4685ffe82ea38c08d17
crynobone <crynobone@gmail.com>
2014-05-14 18:21:17 +0800

    Fixes use statement.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Model/User.php
+++ src/Model/User.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model as Eloquent;
