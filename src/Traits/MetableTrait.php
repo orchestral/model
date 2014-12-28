0346d7f14725a440bd3f5119142f5c2cdd016a93
crynobone <crynobone@gmail.com>
2014-12-28 14:58:10 +0800

    Add MetableTrait.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Traits/MetableTrait.php
@@ -0,0 +1,33 @@
+<?php namespace Orchestra\Model\Traits;
+
+use Orchestra\Model\Value\Meta;
+
+trait MetableTrait
+{
+    /**
+     * `meta` field accessor.
+     *
+     * @param  mixed  $value
+     * @return \Orchestra\Model\Value\Meta
+     */
+    public function getMetaAttribute($value)
+    {
+        $meta = null;
+        if (! is_null($value)) {
+            $meta = json_decode($value, true);
+        }
+        return new Meta($meta);
+    }
+    /**
+     * `meta` field mutator.
+     *
+     * @param  \Orchestra\Model\Value\Meta|null  $value
+     * @return void
+     */
+    public function setMetaAttribute(Meta $value = null)
+    {
+        if (! is_null($value)) {
+            $this->attributes['meta'] = $value->toJson();
+        }
+    }
+}
---
+++ src/Value/Meta.php
@@ -0,0 +1,8 @@
+<?php namespace Orchestra\Model\Value;
+
+use Illuminate\Support\Fluent;
+
+class Meta extends Fluent
+{
+    //
+}
