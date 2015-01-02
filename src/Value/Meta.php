d550ad704ce90438730e4ed7061c43ff9250b6f7
crynobone <crynobone@gmail.com>
2015-01-02 18:49:44 +0800

    Tweak how setter and getter are handled.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Value/Meta.php
+++ src/Value/Meta.php
@@ -1,8 +1,33 @@
 <?php namespace Orchestra\Model\Value;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Fluent;
 
 class Meta extends Fluent
 {
-    //
+    /**
+     * Get an attribute from the container.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        return Arr::get($this->attributes, $key, $default);
+    }
+
+    /**
+     * Set a value from a key.
+     *
+     * @param  string  $key    A string of key to add the value.
+     * @param  mixed   $value  The value.
+     * @return $this
+     */
+    public function put($key, $value = '')
+    {
+        Arr::set($this->attributes, $key, $value);
+
+        return $this;
+    }
 }
