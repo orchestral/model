d89490ba19c777d2e104b7db298f276db6a8db9a
crynobone <crynobone@gmail.com>
2015-09-20 21:00:29 +0800

    Dynamically get foreign key value.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Traits/OwnsTrait.php
@@ -0,0 +1,23 @@
+<?php namespace Orchestra\Model\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait OwnsTrait
+{
+    /**
+     * Check if related model actually owns the relationship.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $related
+     * @param  string|null  $foreignKey
+     *
+     * @return bool
+     */
+    public function owns(Model $related, $foreignKey = null)
+    {
+        if (is_null($foreignKey)) {
+            $foreignKey = $this->getForeignKey();
+        }
+
+        return $relation->getAttribute($foreignKey) == $this->getKey();
+    }
+}
