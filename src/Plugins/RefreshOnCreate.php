cfb8a2ff2bade1a7427bb328b989fa87f47f563a
crynobone <crynobone@gmail.com>
2015-10-05 07:29:03 +0800

    Add RefreshOnCreate plugin.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Plugins/RefreshOnCreate.php
@@ -0,0 +1,33 @@
+<?php namespace Orchestra\Model\Plugins;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait RefreshOnCreate
+{
+    /**
+     * Boot the refresh on create trait for a model.
+     *
+     * @return void
+     */
+    public static function bootRefreshOnCreate()
+    {
+        static::created(function ($model) {
+            static::refresh($model);
+        });
+    }
+
+    /**
+     * Refresh the current model with the current attributes from the database.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    protected static function refresh(Model $model)
+    {
+        $fresh = $model->fresh();
+
+        $model->setRawAttributes($fresh->getAttributes());
+
+        $model->setRelations($fresh->getRelations());
+    }
+}
