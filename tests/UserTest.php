6cde925f38f3e339876b9b79e3fbbab639aa527f
crynobone <crynobone@gmail.com>
2016-06-24 08:40:56 +0800

    Clean up User contracts.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -352,45 +352,6 @@ class UserTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Test Orchestra\Model\User::getEmailForPasswordReset() method.
-     *
-     * @test
-     */
-    public function testGetEmailForPasswordResetMethod()
-    {
-        $stub = new User();
-        $stub->email = 'admin@orchestraplatform.com';
-
-        $this->assertEquals('admin@orchestraplatform.com', $stub->getEmailForPasswordReset());
-    }
-
-    /**
-     * Test Orchestra\Model\User::getRecipientEmail() method.
-     *
-     * @test
-     */
-    public function testGetRecipientEmailMethod()
-    {
-        $stub = new User();
-        $stub->email = 'admin@orchestraplatform.com';
-
-        $this->assertEquals('admin@orchestraplatform.com', $stub->getRecipientEmail());
-    }
-
-    /**
-     * Test Orchestra\Model\User::getRecipientName() method.
-     *
-     * @test
-     */
-    public function testGetRecipientNameMethod()
-    {
-        $stub = new User();
-        $stub->fullname = 'Administrator';
-
-        $this->assertEquals('Administrator', $stub->getRecipientName());
-    }
-
-    /**
      * Test Orchestra\Model\User::activate() method.
      *
      * @test
