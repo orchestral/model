49a693b7a78ec94e584a720ca6ca7e64fb3377b8
Mior Muhammad Zaki <crynobone@gmail.com>
2018-02-20 23:49:23 +0800

    Add UserAccess.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ src/Listeners/UserAccess.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Orchestra\Model\Listeners;
+
+use Orchestra\Model\User;
+use Illuminate\Support\Collection;
+
+class UserAccess
+{
+    /**
+     * Match current user to roles.
+     *
+     * @param  \Orchestra\Model\User|null  $user
+     *
+     * @return \Illuminate\Support\Collection|null
+     */
+    public function handle(?User $user): ?Collection
+    {
+        // When user is null, we should expect the roles is not available.
+        // Therefore, returning null would propagate any other event
+        // listeners (if any) to try resolve the roles.
+
+        return ! is_null($user) ? $user->getRoles() : null;
+    }
+}
